e4ec00ecf3a813ce871900dca65df1aa0f30769a
Francois pied <piedfrancois@laposte.net>
2020-12-03 14:09:53 +0100

    J'ai réussi mon jeu c'est un peu le bordel mais temps pis

--- jeu_de_role.py
+++ jeu_de_role.py
@@ -1,6 +1,6 @@
 import random
 
-potions = 0
+potions = 3
 points_de_vie = 50
 player_1 = player_2 = points_de_vie
 attaque_player_1 = random.randint(5, 10)
@@ -16,21 +16,19 @@ while player_1 != 0 or player_2 != 0:
         attaque = int(attaque)
         if attaque == 1:
             player_2 = player_2 - attaque_player_1
-            print(f"Le score de player 1 dans l'attaque est de {player_1}")
             player_1 = player_1 - attaque_player_2
+            print(f"Le score de player 1 dans l'attaque est de {player_1}")
             print(f"Le score de player 2 dans l'attaque est de {player_2}")
             print(f"Le player 1 à {potions} de potion")
         if attaque == 2:
-            while potions > 0:
-                potions += 1
-                if potions == 0:
-                    player_1 = player_1 + random.randint(15, 50)
-                    player_1 = player_1 - attaque_player_2
-                    print(f"le score de player un avec la potion est de {player_1}")
-                    print(f"le score de player 2 qui n'a pas du changer {player_2}")
-                    print(f"Le player 1 il lui reste {potions} potions")
-                elif potions == 3:
-                    print("Vous n'avez plus de potions")       
+            if potions > 0:
+                potions -= 1
+                potion = random.randint(15, 50)
+                player_1 = player_1 + potion - attaque_player_2
+                print(f"Le score de player 1 est de {player_1}")
+                print(f"Le score de player 2 est de {player_2}")
+                print(f"Vous avez utiliser {potion} de potion")
+                print(f"Il vous reste {potions} de potions")                
         if player_1 <=0 or player_2 <= 0:
             if player_1 <= 0:
                 print("Vous avez perdu")
